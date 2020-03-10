9fdbcd7c748729c2631051e0eff9040f213ff85a
Patrick <p1valtiner@edu.aau.at>
2020-03-10 18:44:21 +0100

    Buttons gemacht

--- app/src/main/java/com/example/einzelabgabe2020/MainActivity.java
+++ app/src/main/java/com/example/einzelabgabe2020/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.einzelabgabe2020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    TextView txt;
+    Button btn;
+    TextView txtAusgabe;
+    TextView txtNummer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt= (TextView) findViewById(R.id.txt);
+        btn= (Button)findViewById(R.id.btn);
+        txtAusgabe= (TextView) findViewById(R.id.txtAusgabe);
+        txtNummer= (TextView) findViewById(R.id.txtNummer);
     }
 }
